e5997f884655beb362f90b56e19ba64d499a6bd2
Fernando Marson <fernando.marson@gmail.com>
2024-10-07 12:46:33 +0100

    Fixed several clients

--- 2024-2/Distributed_Programming/Class_05/version_3/server_3/server_3.cpp
+++ 2024-2/Distributed_Programming/Class_05/version_3/server_3/server_3.cpp
@@ -3,6 +3,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <thread>  // For using threads
+#include <vector>  // For using vectors
 
 #pragma comment(lib, "ws2_32.lib")  // Link the Winsock library
 
@@ -15,16 +16,25 @@ void handleClient(SOCKET clientSocket, int clientID) {
     char buffer[1024] = { 0 };
 
     // Send the client ID
-    string idMessage = "Your client ID is: " + to_string(clientID);
+    string idMessage = "Your client ID is: " + to_string(clientID) + "\n";
     send(clientSocket, idMessage.c_str(), idMessage.length(), 0);
 
-    // Read data from the client
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Message received from client [" << clientID << "]: " << buffer << endl;
+    while (true) {
+        // Read data from the client
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesReceived <= 0) {
+            cout << "Client [" << clientID << "] disconnected." << endl;
+            break;  // Exit loop if no data received or an error occurs
+        }
 
-    // Send response to the client
-    const char* response = "Hello from the server!";
-    send(clientSocket, response, strlen(response), 0);
+        // Null-terminate the received data for safe printing
+        buffer[bytesReceived] = '\0';
+        cout << "Message received from client [" << clientID << "]: " << buffer << endl;
+
+        // Send response to the client
+        const char* response = "Hello from the server!\n";
+        send(clientSocket, response, strlen(response), 0);
+    }
 
     closesocket(clientSocket);  // Close the client socket
 }
@@ -73,17 +83,17 @@ int main() {
     cout << "Waiting for connections..." << endl;
 
     while (true) {
-        SOCKET newSocket = accept(serverSocket, (struct sockaddr*)&address, &addrlen);
-        if (newSocket == INVALID_SOCKET) {
+        SOCKET clientSocket = accept(serverSocket, (struct sockaddr*)&address, &addrlen);
+        if (clientSocket == INVALID_SOCKET) {
             cerr << "Failed to accept connection" << endl;
             continue;  // Continue accepting more clients
         }
 
         clientIDCounter++; // Increment the ID counter
-        thread clientThread(handleClient, newSocket, clientIDCounter); // Create a new thread for each client
-        clientThread.detach();  // Detach the thread to allow it to run independently
+        thread(handleClient, clientSocket, clientIDCounter).detach(); // Create a new thread for each client
     }
 
+    cout << "Closing all connections..." << endl;
     closesocket(serverSocket);
     WSACleanup();
 
