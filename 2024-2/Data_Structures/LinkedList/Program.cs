076d95b4b000a74f7fbce537f4475bdc8a98022b
Fernando Marson <fernando.marson@gmail.com>
2024-11-18 13:51:13 +0000

    Add files via upload

---
+++ 2024-2/Data_Structures/LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LinkedList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.AddLast(10);
+        list.AddLast(20);
+        list.AddLast(30);
+
+        Console.WriteLine("List:");
+        list.PrintBackward();
+
+        Console.ReadLine();
+    }
+}
